ba6127123e8ff663ea70aaad7486c015678f7656
Alexander <zambrana046@gmail.com>
2026-03-13 09:40:26 -0600

    Pagina inicial

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Cora Web</h1>
+      <p>Plantilla inicial de React</p>
+    </div>
+  )
+}
+
+export default App
