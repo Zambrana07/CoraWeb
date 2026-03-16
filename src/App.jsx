4fcb6e376e588fd57a5a5d3b67c183d9d07520f2
Alexander <zambrana046@gmail.com>
2026-03-16 14:47:02 -0600

    Iniciar

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="app">
       <h1>Cora Web</h1>
-      <p>Plantilla inicial de React</p>
+      <p>Plantilla inicial de Cora, el header  y el footer se crearan en el futuro/por separado</p>
     </div>
   )
 }
