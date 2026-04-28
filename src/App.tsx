82561c81d26916e82922339cda64645a530650f8
Louelena <lourdeschinchillaroman@gmail.com>
2026-04-28 08:55:10 -0600

    Bare minimun

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import Index from "./pages/Index.tsx";
 import ArchiveroPage from "./pages/ArchiveroPage.jsx";
 import Perfil from "./pages/perfil.jsx";
