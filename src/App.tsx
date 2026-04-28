b6595b716c7632ac567d1ba8d97bbad549bfedf8
Louelena <lourdeschinchillaroman@gmail.com>
2026-04-28 08:56:43 -0600

    More minimum

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,6 @@ import NotFound from "./pages/NotFound.tsx";
 const queryClient = new QueryClient();
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
       <BrowserRouter>
       <Routes>
           <Route path="/" element={<Index />} />
@@ -21,9 +17,6 @@ const App = () => (
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
-        
-    </TooltipProvider>
-  </QueryClientProvider>
 );
 
 export default App;
